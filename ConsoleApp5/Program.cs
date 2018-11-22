dcf2c7fce8a2c3e45acbe8de8074461f8b28e20b
alex <alex_samarkin@mail.ru>
2018-11-23 00:44:35 +0300

    Добавлены финансы игрока и компьютера

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+        }
+
+        // деньги на игру - поля класса
+        private static int Player = 100;
+        private static int Comp = 100;
+
+        // печать статуса
+        static void PrintStatus()
+        {
+            Console.WriteLine("Что у нас с финансами?");
+            Console.WriteLine($"У вас - {Player} руб., у компьютера - {Comp} руб.");
         }
     }
 }
